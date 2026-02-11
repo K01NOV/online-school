a2334cc6d277e16ad171fe4195cbfec8b7c3069b
K01NOV <cubegreen7@gmail.com>
2026-02-11 19:33:14 +0700

    Delete App/Config/config.php

--- App/Config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'yandex' => [
-        'client_id' => '387bce03ad0943d69c0ba29820327f7e',
-        'client_secret' => 'e8f4cca6041c4d8b8575801163612ffb',
-        'token'  => 'y0__xCNrYDPBxjvvD0gwI7MpRYwm-OpzwhGBMvhiFtM_HtMESKSkvF8od1rWA'
-    ]
-];
