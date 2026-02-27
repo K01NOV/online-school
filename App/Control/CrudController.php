8ac70c1820c3f880eb3f00d0f1dabb46be18abd8
K01NOV <cubegreen7@gmail.com>
2026-02-27 20:54:31 +0700

    update CrudController with integrating CrudModel

--- App/Control/CrudController.php
+++ App/Control/CrudController.php
@@ -1,13 +1,22 @@
 <?php namespace App\Control;
 
+use App\Model\CrudModel;
+
 class CrudController{
     private $conn;
+    private $crudModel;
 
     public function __construct($db){
         $this->conn = $db;
+        $this->crudModel = new CrudModel($this->conn);
     }
 
     public function show_crud(){
+        $tables = $this->crudModel->get_all_tables();
+        if(isset($_GET['table'])){
+            $columns = $this->crudModel->get_columns($_GET['table']);
+            $data = $this->crudModel->get_data($_GET['table']);
+        }
         require_once __DIR__ . '/../../View/admin_head.php';
         require_once __DIR__ . '/../../View/admin_crud.php';
     }
