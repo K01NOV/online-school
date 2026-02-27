6e57644960f64848e4d6922ec8d481bebced99cf
K01NOV <cubegreen7@gmail.com>
2026-02-27 20:53:17 +0700

    refactor admin_crud.php and styles.css by correcting layout

--- View/admin_crud.php
+++ View/admin_crud.php
@@ -1,8 +1,8 @@
-<body class="bo-body">
+<div class="bo-body">
     <div class="bo-container">
         <?php require_once __DIR__ . "/tables_list.php" ?>
     </div>
     <main class="bo-main-content">
         <?php require_once __DIR__ . "/table.php" ?>
     </main>
-</body>
+</div>
