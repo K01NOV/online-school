557e9ba28d4ea33a794c79d2bc156eda3de1a1f5
K01NOV <cubegreen7@gmail.com>
2026-02-27 20:53:55 +0700

    add CrudModel.php with functions: get all tables, get columns, get data, delete row and insert into

---
+++ App/Model/CrudModel.php
@@ -0,0 +1,108 @@
+<?php namespace App\Model;
+
+use PDO;
+use Exception;
+class CrudModel{
+    private $conn;
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    public function get_all_tables(){
+        $sql = "SHOW TABLES FROM online_school";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $tables = [];
+        foreach($rows as $row){
+            $tables[] = $row['Tables_in_online_school'];
+        }
+        return $tables;
+    }
+
+    public function get_columns($table){
+        $tables = $this->get_all_tables();
+        if(!in_array($table, $tables)){
+            throw new \Exception("Таблица не найдена: " . $table);
+        }
+        $sql = "SHOW COLUMNS FROM online_school.`$table`";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $columns = [];
+        foreach($rows as $row){
+            $field = $row['Field'];
+            $rawType = $row['Type']; 
+
+            if (str_contains($rawType, 'int')) {
+                $type = "number";
+            } elseif (str_contains($rawType, 'date')) {
+                $type = "date";
+            } else {
+                $type = "text";
+            }
+
+            $columns[$field] = [
+                'type' => $type,
+                'required' => ($row['Null'] === "NO") ? "required" : "",
+                'disabled' => ($row['Extra'] === 'auto_increment') ? "disabled" : ""
+            ];
+        }
+
+        return $columns;
+    }
+
+    public function get_data($table){
+        $tables = $this->get_all_tables();
+        if(!in_array($table, $tables)){
+            throw new \Exception("Таблица не найдена: " . $table);
+        }
+        $sql = "SELECT DISTINCT * FROM `$table`";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $rows;
+    }
+
+    public function delete_row($table, int $id){
+        $tables = $this->get_all_tables();
+        if(!in_array($table, $tables)){
+            throw new \Exception("Таблица не найдена: " . $table);
+        }
+
+        $sql = "DELETE FROM `$table` WHERE `$table`.`id` = :id";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(":id", $id, PDO::PARAM_INT);
+        if($stmt->execute()){
+            return true;
+        }
+        
+        throw new \Exception("Возникла ошибка при удалении");
+    }
+
+    public function insert_into($table, $data){
+        $tables = $this->get_all_tables();
+        if (!in_array($table, $tables)) {
+            throw new \Exception("Таблица не найдена");
+        }
+
+        if (empty($data)) {
+            throw new \Exception("Недостаточно данных");
+        }
+
+        $columns = array_keys($data);
+        $colString = implode(", ", array_map(function($col){ return "`$col`"; }, $columns));
+        $placeholders = ":" . implode(", :", $columns);
+        $sql = "INSERT INTO `$table` ($colString) VALUES ($placeholders)";
+        $stmt = $this->conn->prepare($sql);
+        foreach ($data as $key => $value) {
+            $stmt->bindValue(":$key", $value);
+        }
+        if ($stmt->execute()) {
+            return $this->conn->lastInsertId(); // Возвращаем id
+        }
+
+        throw new \Exception("Ошибка при вставке данных");
+    }
+}
