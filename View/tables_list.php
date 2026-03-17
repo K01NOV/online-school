8ec6a70b5edfe94be8d642c9088d57db3fd4d9db
K01NOV <cubegreen7@gmail.com>
2026-03-17 23:09:25 +0700

    fix navigation links

--- View/tables_list.php
+++ View/tables_list.php
@@ -1,5 +1,5 @@
 <nav class="bo-side-nav">
     <?php if(isset($tables)): foreach($tables as $table): ?>
-        <a href="?table=<?= urlencode($table) ?>" class="bo-nav-link <?= (isset($_GET['table']) && $_GET['table'] == $table) ? 'active' : '' ?>"><?= htmlspecialchars($table) ?></a>
+        <a href="?<?php if(isset($_GET['mode'])) echo "mode=" . $_GET['mode'] . '&'?>table=<?= urlencode($table) ?>" class="bo-nav-link <?= (isset($_GET['table']) && $_GET['table'] == $table) ? 'active' : '' ?>"><?= htmlspecialchars($table) ?></a>
     <?php endforeach; endif;?>
 </nav>
