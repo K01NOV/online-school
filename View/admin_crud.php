20ef859a3bcb11bec7e8f57b3d76ff626be71a12
K01NOV <cubegreen7@gmail.com>
2026-03-17 23:07:40 +0700

    add navigation tabs

--- View/admin_crud.php
+++ View/admin_crud.php
@@ -1,8 +1,18 @@
+<main class="dashboard-content">
+    <nav class="navigation-row">
+        <a href="?mode=table<?php if(isset($_GET['table'])) echo "&table=" . $_GET['table']?>" class="nav-btn"><h2>Таблицы</h2></a>
+        <a href="?mode=query<?php if(isset($_GET['table'])) echo "&table=" . $_GET['table']?>" class="nav-btn"><h2>Запросы</h2></a>
+        <a href="?mode=construct<?php if(isset($_GET['table'])) echo "&table=" . $_GET['table']?>" class="nav-btn"><h2>Конструктор</h2></a>
+    </nav>
+</main>
 <div class="bo-body">
     <div class="bo-container">
         <?php require_once __DIR__ . "/tables_list.php" ?>
     </div>
     <main class="bo-main-content">
-        <?php require_once __DIR__ . "/table.php" ?>
+        <?php if(isset($_GET['mode']) && $_GET['mode'] == 'table')
+            require_once __DIR__ . "/table.php"?>
+        <?php if(isset($_GET['mode']) && $_GET['mode'] == 'construct')
+            require_once __DIR__ . "/construct.php"?>
     </main>
 </div>
