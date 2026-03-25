ac6f413d4db4995d5ffaaf88e155f42de495a91f
K01NOV <cubegreen7@gmail.com>
2026-03-25 14:22:56 +0700

    add dynamic subject loading in landing page in PagesController

--- App/Control/PagesController.php
+++ App/Control/PagesController.php
@@ -1,8 +1,32 @@
 <?php namespace App\Control;
 
+use App\Model\SubjectModel;
+use App\Control\SubjectController;
+use App\Model\TopicModel;
+
 class PagesController{
-    function __construct($db){}
+    private $subjectModel;
+    private $conn;
+    private $subjectController;
+    private $topicModel;
+    function __construct($db){
+        $this->conn = $db;
+        $this->subjectModel = new SubjectModel($this->conn);
+        $this->subjectController = new SubjectController($this->conn);
+        $this->topicModel = new TopicModel($this->conn);
+    }
     function showHome(){
+        $subjects = $this->subjectModel->get_all();
+        $realSubjects = [];
+        foreach ($subjects as $subject) {
+            $topics = $this->topicModel->get_all($subject->id);
+            if(count($topics) > 0){
+                $subject->image = $this->subjectController->refresh_link($subject);
+                $realSubjects[] = $subject;
+            }
+        }
+        $subjects = $realSubjects;
+        
         require_once __DIR__ . "/../../View/head_home.php";
         require_once __DIR__ . "/../../View/home.php";
         require_once __DIR__ . "/../../View/footer.php";
