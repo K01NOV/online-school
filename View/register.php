da0fcd95c3b4368c6ee224b6d256573e768318c0
K01NOV <cubegreen7@gmail.com>
2026-03-30 22:29:39 +0700

    translate auth forms to ru

--- View/register.php
+++ View/register.php
@@ -1,26 +1,23 @@
 <div class="reg-auth-container">
     <form class="reg-auth-form" action="/register" method="POST">
-        <h3>Registration</h3>
+        <h3>Регистрация</h3>
         <?php if(isset($_SESSION['error'])): ?>
             <p style="color: #f0785d"><?php echo htmlspecialchars($_SESSION['error']); unset($_SESSION['error']);?></p>
         <?php endif?>
-        <input type="text" placeholder="name" name="name" >
-        <input type="email" placeholder="email" name="email">
-        <input type="text" placeholder="nickname" name="nick" id="nick">
-        <select name="account_type" id="selector">
+        <input type="text" placeholder="Имя" name="name" >
+        <input type="email" placeholder="Почта" name="email">
+        <input type="text" placeholder="Юзернейм" name="nick" id="nick">
+        <select name="account_type" id="selector" readonly>
             <option value="Личный аккаунт">Личный аккаунт</option>
-            <option value="Ученик">Ученик</option>
-            <option value="Учитель">Учитель</option>
-            <option value="Родитель">Родитель</option>
         </select>
-        <input type="password" placeholder="password" name="password">
+        <input type="password" placeholder="Пароль" name="password">
         <input type="submit">
         <input type="hidden" name="token" value="<?= htmlspecialchars($_SESSION['auth_token'])?>" style="display: none">
     </form>
     <form class="reg-auth-form" action="/login" method="POST">
-        <h3>Authorization</h3>
-        <input type="email or nickname" placeholder="email" name="login">
-        <input type="password" placeholder="password" name="password">
+        <h3>Войти</h3>
+        <input type="text" placeholder="Почта или Юзернейм" name="login">
+        <input type="password" placeholder="Пароль" name="password">
         <input type="submit">
         <input type="hidden" name="token" value="<?= htmlspecialchars($_SESSION['auth_token'])?>" style="display: none">
     </form>
